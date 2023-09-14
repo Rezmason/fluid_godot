78df44107c83aba43c8d4bf9469daf2f40d1856e
Rezmason <jeremysachs@rezmason.net>
2023-09-14 02:38:21 -0700

    Replaced Alga with a Lilypad class, which is arranged in a hexagonal grid and contains a set of all neighboring Lilypads

--- Game.cs
+++ Game.cs
@@ -4,29 +4,66 @@ using System.Collections.Generic;
 
 public partial class Game : Node2D
 {
-	private class Alga {
+	private class Lilypad {
 		public Node2D scene;
+		public Node2D alga;
+		public Node2D muck;
+		public Label label;
+		public HashSet<Lilypad> neighbors = new HashSet<Lilypad>();
+
+		static PackedScene algaArt = (PackedScene)ResourceLoader.Load("res://alga.tscn");
+		static PackedScene muckArt = (PackedScene)ResourceLoader.Load("res://muck.tscn");
+
 		public bool fed = false;
 		public bool mucked = false;
-		public List<Alga> neighbors = new List<Alga>();
 
-		public Alga(Node2D scene) {
-			this.scene = scene;
+		public Lilypad() {
+			scene = new Node2D();
+			muck = (Node2D)muckArt.Instantiate();
+			scene.AddChild(muck);
+			muck.Visible = false;
+			alga = (Node2D)algaArt.Instantiate();
+			scene.AddChild(alga);
+			label = new Label();
+			label.LabelSettings = new LabelSettings{FontColor = new Color("black")};
+			scene.AddChild(label);
 		}
 	}
 
-	List<Alga> algae = new List<Alga>();
+	List<List<Lilypad>> lilypads = new List<List<Lilypad>>();
 
 	public override void _Ready()
 	{
-		var algaScene = ResourceLoader.Load("res://alga.tscn") as PackedScene;
-		
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				var alga = new Alga(algaScene.Instantiate() as Node2D);
-				algae.Add(alga);
-				alga.scene.Position = new Vector2(i * 20, j * 20);
-				this.AddChild(alga.scene);
+		const int numRows = 9;
+		int[] numColumns = {10, 9};
+		var spacing = new Vector2(110, 90);
+		for (int i = 0; i < numRows; i++) {
+			var rowOffset = new Vector2(1 - numColumns[i % 2], 1 - numRows) / 2;
+			var row = new List<Lilypad>();
+			lilypads.Add(row);
+			for (int j = 0; j < numColumns[i % 2]; j++) {
+				var lilypad = new Lilypad();
+				lilypad.scene.Position = (new Vector2(j, i) + rowOffset) * spacing;
+				row.Add(lilypad);
+				AddChild(lilypad.scene);
+				// lilypad.label.Text = $"{i},{j}";
+			}
+		}
+
+		void ConnectNeighbors(Lilypad l1, Lilypad l2) {
+			l1.neighbors.Add(l2);
+			l2.neighbors.Add(l1);
+		}
+
+		for (int i = 0; i < numRows; i++) {
+			for (int j = 1; j < numColumns[i % 2]; j++) {
+				ConnectNeighbors(lilypads[i][j], lilypads[i][j - 1]);
+				if (i > 0) {
+					ConnectNeighbors(lilypads[i][j], lilypads[i - 1][j - ((i + 1) % 2)]);
+				}
+				if (i < numRows - 1) {
+					ConnectNeighbors(lilypads[i][j], lilypads[i + 1][j - ((i + 1) % 2)]);
+				}
 			}
 		}
 	}
