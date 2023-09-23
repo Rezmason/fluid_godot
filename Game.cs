047d180ab2fbf48e3de477eeb94955c613570076
Rezmason <jeremysachs@rezmason.net>
2023-09-23 11:48:49 -0700

    Overhauling the logic within Creature to return earlier and to iterate over a list of elements that includes itself.

--- Game.cs
+++ Game.cs
@@ -278,6 +278,7 @@ public partial class Game : Node2D
 	
 	private class Feeder
 	{
+		const int maxAvailableSeeds = 40;
 		const float minSeedDist = 100;
 		const float minDist = 80;
 		const float margin = 50;
@@ -286,10 +287,10 @@ public partial class Game : Node2D
 		public Node2D scene;
 		public Node2D art;
 		private List<Feeder> children = new List<Feeder>();
-		private List<Node2D> arts = new List<Node2D>();
+		private List<Feeder> elements = new List<Feeder>();
 		public Feeder parent;
 		public Vector2 velocity = Vector2.Zero;
-		public int Size => children.Count + 1;
+		public int Size => elements.Count;
 		
 		static PackedScene feederArt = ResourceLoader.Load<PackedScene>("res://feeder.tscn");
 		
@@ -307,8 +308,8 @@ public partial class Game : Node2D
 				child.parent = null;
 			}
 			children.Clear();
-			arts.Clear();
-			arts.Add(art);
+			elements.Clear();
+			elements.Add(this);
 			art.Position = Vector2.Zero;
 			parent = null;
 			velocity = Vector2.Zero;
@@ -320,47 +321,51 @@ public partial class Game : Node2D
 		{
 			if (Size < 3 || availableSeeds <= 0) return false;
 			var minSeedDistSquared = minSeedDist * minSeedDist;
-			if (scene.GlobalPosition.DistanceSquaredTo(lilypad.scene.GlobalPosition) < minSeedDistSquared) {
-				lilypad.RipenAlga();
-				// TODO: grow transparent
-				availableSeeds--;
-				if (availableSeeds <= 0) Burst();
-				return true;
+			if (scene.GlobalPosition.DistanceSquaredTo(lilypad.scene.GlobalPosition) > minSeedDistSquared) {
+				return false;
 			}
-			return false;
+
+			lilypad.RipenAlga();
+			availableSeeds--;
+			if (availableSeeds <= 0) {
+				Burst();
+			} else {
+				foreach (var feeder in elements) {
+					var art = feeder.art;
+					// TODO: tween art transparency do availableSeeds / maxAvailableSeeds
+				}
+			}
+			return true;
 		}
 		
 		private void Burst()
 		{
-			const float burstPower = 6;
 			var oldPosition = scene.GlobalPosition;
 			var artPositions = new List<Vector2>();
-			foreach (var art in arts) {
-				artPositions.Add(art.GlobalPosition);
-				// TODO: reset transparency
+			foreach (var feeder in elements) {
+				artPositions.Add(feeder.art.GlobalPosition);
 			}
-			
-			scene.GlobalPosition = artPositions[0];
-			velocity = (artPositions[0] - oldPosition) * burstPower;
-			age = 0;
 			
 			var parentNode = scene.GetParent();
-			for (int i = 1; i < 3; i++) {
-				var child = children[i - 1];
+			foreach (var child in children) {
 				scene.RemoveChild(child.scene);
-				parentNode.AddChild(child.scene);
 				child.parent = null;
-				child.scene.GlobalPosition = artPositions[i];
-				child.velocity = (artPositions[i] - oldPosition) * burstPower;
+				parentNode.AddChild(child.scene);
 			}
-			
-			foreach (var art in arts) {
-				art.Position = Vector2.Zero;
-			}
-			
 			children.Clear();
-			arts.Clear();
-			arts.Add(art);
+			
+			for (int i = 0; i < 3; i++) {
+				var feeder = elements[i];
+				feeder.age = 0;
+				feeder.scene.GlobalPosition = artPositions[i];
+				feeder.velocity = (artPositions[i] - oldPosition) * 6;
+				var art = feeder.art;
+				art.Position = Vector2.Zero;
+				// TODO: tween art transparency to one
+			}
+			elements.Clear();
+			elements.Add(this);
+			
 			availableSeeds = 0;
 		}
 		
@@ -370,44 +375,41 @@ public partial class Game : Node2D
 			
 			const float minDistSquared = minDist * minDist;
 			var otherGlobalPosition = other.art.GlobalPosition;
-			if (art.GlobalPosition.DistanceSquaredTo(otherGlobalPosition) < minDistSquared) {
-				if (Size == 2) {
-					if (arts[1].GlobalPosition.DistanceSquaredTo(otherGlobalPosition) > minDistSquared) {
-						return false;
-					}
+
+			foreach (var feeder in elements) {
+				if (feeder.art.GlobalPosition.DistanceSquaredTo(otherGlobalPosition) > minDistSquared) {
+					return false;
 				}
-				
-				children.Add(other);
-				arts.Add(other.art);
-				other.parent = this;
-				other.age = 0;
-				
-				var averageGlobalPosition = Vector2.Zero;
-				var artPositions = new List<Vector2>();
-				foreach (var art in arts) {
-					averageGlobalPosition += art.GlobalPosition;
-					artPositions.Add(art.GlobalPosition);
-				}
-				averageGlobalPosition /= Size;
-				
-				scene.GlobalPosition = averageGlobalPosition;
-				other.scene.GetParent().RemoveChild(other.scene);
-				scene.AddChild(other.scene);
-				other.scene.Position = Vector2.Zero;
-				
-				for (int i = 0; i < Size; i++) {
-					arts[i].GlobalPosition = artPositions[i];
-				}
-				
-				velocity = (velocity * (Size - 1) + other.velocity) / Size;
-				other.velocity = Vector2.Zero;
-				
-				if (Size == 3) availableSeeds = 40;
-				
-				return true;
-			} else {
-				return false;
 			}
+
+			children.Add(other);
+			elements.Add(other);
+			other.parent = this;
+
+			velocity = (velocity * (Size - 1) + other.velocity) / Size;
+			other.velocity = Vector2.Zero;
+			other.age = 0;
+
+			if (Size == 3) availableSeeds = maxAvailableSeeds;
+
+			var averageGlobalPosition = Vector2.Zero;
+			var artPositions = new List<Vector2>();
+			foreach (var feeder in elements) {
+				averageGlobalPosition += feeder.art.GlobalPosition;
+				artPositions.Add(feeder.art.GlobalPosition);
+			}
+			averageGlobalPosition /= Size;
+
+			scene.GlobalPosition = averageGlobalPosition;
+			other.scene.GetParent().RemoveChild(other.scene);
+			scene.AddChild(other.scene);
+			other.scene.Position = Vector2.Zero;
+
+			for (int i = 0; i < Size; i++) {
+				elements[i].art.GlobalPosition = artPositions[i];
+			}
+
+			return true;
 		}
 		
 		public void Update(float delta)
@@ -439,13 +441,19 @@ public partial class Game : Node2D
 			}
 			
 			if (Size == 2) {
-				foreach (var art in arts) {
+				foreach (var feeder in elements) {
+					var art = feeder.art;
 					var goalPosition = art.Position * (minDist / 2) / art.Position.Length();
 					art.Position = art.Position.Lerp(goalPosition, 0.2f);
 				}
 			} else if (Size == 3) {
-				var averagePosition = (arts[0].Position + arts[1].Position + arts[2].Position) / 3;
-				foreach (var art in arts) {
+				var averagePosition = (
+					elements[0].art.Position +
+					elements[1].art.Position +
+					elements[2].art.Position
+				) / 3;
+				foreach (var feeder in elements) {
+					var art = feeder.art;
 					var goalPosition = art.Position - averagePosition;
 					goalPosition *= (minDist / 2) / goalPosition.Length();
 					art.Position = art.Position.Lerp(goalPosition, 0.2f);
